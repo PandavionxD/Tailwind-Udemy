63b2b8a51d75b92f720f2251e89e4ae592e2dc6b
Alex Daniel <1151372@senati.pe>
2023-05-18 15:42:06 -0500

    Creando en Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode:false,
+  theme: {
+    extend: {
+      container:{
+        center:true
+      }
+    },
+  },
+  plugins: [],
+}
+
